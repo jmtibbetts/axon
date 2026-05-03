fcb521630bbcc761c255b0134ff9282cbc55bbf1
AXON <axon@neuralforge.ai>
2026-05-03 08:34:40 +0000

    fix: match AuditorySystem constructor signature (on_speech not on_transcript)

--- axon/core/engine.py
+++ axon/core/engine.py
@@ -55,7 +55,7 @@ class AxonEngine:
             on_frame=self._on_frame,
             on_face=self._on_face,
         )
-        self.auditory = AuditorySystem(on_transcript=self._on_transcript)
+        self.auditory = AuditorySystem(on_speech=self._on_transcript, on_volume=lambda v: None)
 
         self._last_visual_ctx = {}
 
